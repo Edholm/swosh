06428d65c290d6f72e6f43ef30d01010c9716d8f
Emil Edholm <bobby@verypublic.org>
2017-04-06 22:19:27 +0200

    Neater way of generating IDs

--- src/main/kotlin/pub/edholm/db/Swosh.kt
+++ src/main/kotlin/pub/edholm/db/Swosh.kt
@@ -9,12 +9,13 @@ import java.time.Instant
 @Document
 data class Swosh(
         @Id
-        val id: String = BigInteger(28, SecureRandom()).toString(32),
+        val id: String = BigInteger(130, SecureRandom()).toString(32).substring(Swosh.ID_LENGTH),
         val payee: String = "N/A",
         val amount: Int = 1,
         val description: String? = null,
         val expiresOn: Instant = Instant.now().plusSeconds(Swosh.DEFAULT_EXPIRY_TIME_IN_SECONDS)) {
     companion object {
         const val DEFAULT_EXPIRY_TIME_IN_SECONDS: Long = 172800 // 2 days
+        const val ID_LENGTH = 6
     }
 }
