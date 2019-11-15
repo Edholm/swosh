2979c9592751116b372de5153beb9eb020a71835
Emil Edholm <bobby@verypublic.org>
2019-11-15 22:12:53 +0100

    Build with Gradle before docker

--- buildDockerMulti.sh
+++ buildDockerMulti.sh
@@ -6,27 +6,32 @@ repo="edholm/swosh"
 base_tag="${repo}:${version}"
 latest_tag="${repo}:latest"
 
+function gradlebuild {
+  ./gradlew clean build
+}
+
 function dockerbuild {
     platform="${1}"
     tag="${base_tag}-${platform}"
 
     echo -----------------------------------------
-    echo Starting build of $tag
+    echo Starting build of "$tag"
     docker build --pull=true --platform "${platform}" --squash --tag="${tag}" -f Dockerfile build/libs
     docker push "${tag}"
 }
 
+gradlebuild
 dockerbuild arm64
 dockerbuild amd64
 
-docker manifest create --amend ${base_tag} \
-    ${base_tag}-arm64 \
-    ${base_tag}-amd64
+docker manifest create --amend "${base_tag}" \
+    "${base_tag}"-arm64 \
+    "${base_tag}"-amd64
 docker manifest create --amend ${latest_tag} \
-    ${base_tag}-arm64 \
-    ${base_tag}-amd64
+    "${base_tag}"-arm64 \
+    "${base_tag}"-amd64
 
-docker manifest push ${base_tag}
+docker manifest push "${base_tag}"
 docker manifest push ${latest_tag}
 
 echo "${base_tag} is done!"
