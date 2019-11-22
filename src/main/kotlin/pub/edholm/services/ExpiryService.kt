3e0cd2bd60301b6cc2df06ad5135c65a76cb92cf
Emil Edholm <bobby@verypublic.org>
2019-11-22 20:09:25 +0100

    Metrics on how many links are expired

--- src/main/kotlin/pub/edholm/services/ExpiryService.kt
+++ src/main/kotlin/pub/edholm/services/ExpiryService.kt
@@ -1,16 +1,30 @@
 package pub.edholm.services
 
+import io.micrometer.core.instrument.MeterRegistry
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import pub.edholm.db.SwoshRepository
 import java.time.Instant
 
 @Service
-class ExpiryService(private val swoshRepo: SwoshRepository) {
+class ExpiryService(
+  private val swoshRepo: SwoshRepository,
+  meterRegistry: MeterRegistry
+) {
+
+  private val counter = meterRegistry.counter("expired.links")
+  private val logger = LoggerFactory.getLogger(ExpiryService::class.java)
 
   @Scheduled(fixedRateString = "\${swosh.expire.rate}", initialDelay = 1337)
   fun expireOldLinks() {
-    swoshRepo.deleteAll(swoshRepo.findByExpiresOnBefore(Instant.now()))
-      .subscribe()
+    val expired = swoshRepo.findByExpiresOnBefore(Instant.now())
+    expired.count().subscribe { c ->
+      if (c > 0) {
+        logger.info("Expiring {} links", c)
+      }
+      counter.increment(c.toDouble())
+    }
+    swoshRepo.deleteAll(expired).subscribe()
   }
 }
