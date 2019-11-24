f947ccd404d9f19e677b9658680c0f7f8066fd4d
Emil Edholm <bobby@verypublic.org>
2019-11-24 13:56:21 +0100

    Change actuator port to 8081 by default
    Also remove the security config from it, as well as changing the
    readiness and liveness probes to use the actuator

--- src/main/kotlin/pub/edholm/SecurityConfig.kt
+++ src/main/kotlin/pub/edholm/SecurityConfig.kt
@@ -1,16 +1,11 @@
 package pub.edholm
 
 import org.springframework.context.annotation.Bean
-import org.springframework.security.authorization.AuthorizationDecision
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.web.server.SecurityWebFilterChain
-import org.springframework.security.web.server.authorization.AuthorizationContext
-import reactor.core.publisher.Mono
-import reactor.kotlin.core.publisher.switchIfEmpty
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
@@ -18,8 +13,6 @@ class SecurityConfig {
   @Bean
   fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
     return http.authorizeExchange()
-      .pathMatchers("/actuator/**")
-      .access(this::isFromLocalhostOrAdmin)
       .pathMatchers("/admin/**")
       .hasRole("ADMIN")
       .anyExchange()
@@ -35,13 +28,6 @@ class SecurityConfig {
       .formLogin()
       .and()
       .build()
-  }
-
-  fun isFromLocalhostOrAdmin(mono: Mono<Authentication>, context: AuthorizationContext): Mono<AuthorizationDecision> {
-    val requesterFromLocalhost = context.exchange.request.remoteAddress?.address?.isLoopbackAddress ?: false
-    return mono
-      .map { AuthorizationDecision(requesterFromLocalhost || isAdmin(it.authorities)) }
-      .switchIfEmpty<AuthorizationDecision> { Mono.just(AuthorizationDecision(requesterFromLocalhost)) }
   }
 
   fun isAdmin(authorities: Collection<GrantedAuthority>): Boolean = authorities.any { it.authority == "ROLE_ADMIN" }
