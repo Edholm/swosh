a8a51d6f53be9a85ad60e1cef4fceba6aae9ea50
Emil Edholm <bobby@verypublic.org>
2019-12-05 16:57:10 +0100

    Fix the delete button
    It didn't work because CSP and onClick handler

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,4 +1,12 @@
-function deleteSwosh(swoshId) {
+document.addEventListener('DOMContentLoaded', function () {
+  const buttonElements = document.querySelectorAll('button.delete-btn');
+  for (let i = 0; i < buttonElements.length; i++) {
+    buttonElements[i].addEventListener('click', deleteSwosh);
+  }
+});
+
+function deleteSwosh(event) {
+  let swoshId = event.target.id;
   if (confirm("Are you sure you want to delete " + swoshId + "?")) {
     $.ajax("/admin/" + swoshId, {
       method: 'DELETE'
